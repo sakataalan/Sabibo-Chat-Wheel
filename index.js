f9796ad0f2c02f6998953c5512b28c68bb9ef1d2
Alan Sakata <sakataalan@iMac-Pro-de-Alan.local>
2021-10-15 22:49:08 -0300

    update

--- index.js
+++ index.js
@@ -14,41 +14,48 @@ client.on("ready", () => {
 })
 
 client.on("messageCreate", async msg => {
-    const audio = msg.content[4];
-
-    function isCommand(command) {
-        return !!msg.content.toLowerCase().startsWith(prefix + command);
-    };
     
-    if (isCommand("cw " + audio) && !isNaN(parseInt(audio)) && parseInt(audio) <= textContent.length) {
-        const channel = msg.member.voice.channel;   
-        
-        if(!channel) {
-            return msg.reply(msg.author.username + " me ajuda a te ajudar brother. Tu tem que estar em um canal de voz pra mágica rolar.");
-        }
-        
-        const player = voiceDiscord.createAudioPlayer();
-        const resource = voiceDiscord.createAudioResource(`./Assets/${audio}.mp3`);
-        
-        const connection = voiceDiscord.joinVoiceChannel({
-            channelId: channel.id,
-            guildId: msg.guild.id,
-            adapterCreator: msg.guild.voiceAdapterCreator,
-        });
-        
-        msg.channel.send(`${msg.author.username}: :loud_sound: ${textContent[audio-1].replace(/[0-9]\s-\s/g, "")}`);
+    const audio = msg.content.match(/\d/g);
 
-        player.play(resource);
-        connection.subscribe(player);        
+    if(audio != null){
 
-        player.on(voiceDiscord.AudioPlayerStatus.Idle, ()=>{
-            connection.destroy();
+        const batatinhaFrita123 = audio[0];
+        console.log(batatinhaFrita123);
+        
+        function isCommand(command) {
+            return !!msg.content.toLowerCase().startsWith(prefix + command);
+        };
+        
+        if (isCommand("cw") && !isNaN(parseInt(audio[0])) && parseInt(audio[0]) <= textContent.length) {
+            const channel = msg.member.voice.channel;   
             
-        }) 
-    } else if(isCommand("cw " + "list")){
-        msg.channel.send(textContentToShow);
-    } else if (!msg.author.bot){
-        msg.reply("Não entendi, porra, manda de novo.");
+            if(!channel) {
+                return msg.reply(msg.author.username + " me ajuda a te ajudar brother. Tu tem que estar em um canal de voz pra mágica rolar.");
+            }
+            
+            const player = voiceDiscord.createAudioPlayer();
+            const resource = voiceDiscord.createAudioResource(`./Assets/${audio}.mp3`);
+            
+            const connection = voiceDiscord.joinVoiceChannel({
+                channelId: channel.id,
+                guildId: msg.guild.id,
+                adapterCreator: msg.guild.voiceAdapterCreator,
+            });
+            
+            msg.channel.send(`${msg.author.username}: :loud_sound: ${textContent[audio-1].replace(/[0-9]\s-\s/g, "")}`);
+    
+            player.play(resource);
+            connection.subscribe(player);        
+    
+            player.on(voiceDiscord.AudioPlayerStatus.Idle, ()=>{
+                connection.destroy();
+                
+            }) 
+        } else if(isCommand("cw " + "list")){
+            msg.channel.send(textContentToShow);
+        } else if (!msg.author.bot){
+            msg.reply("Não entendi, porra, manda de novo.");
+        }
     }
 })
 
